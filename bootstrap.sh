2bba60f9ba9008f4d440d6a6ab2da75251a17914
Daniel Pecher <dan.pecher@gmail.com>
2026-01-02 19:04:40 +0100

    add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# install nix
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install)
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
